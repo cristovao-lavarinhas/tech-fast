b00d137329e1ab0f52b3c89f54368dea01ee9c08
The-CrazyMouse <andrerddantas@hotmail.com>
2025-06-02 17:35:29 +0100

    Descontos page

--- src/app/descontos/descontos.module.ts
+++ src/app/descontos/descontos.module.ts
@@ -1,8 +1,12 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
+
+import { HttpClientModule } from '@angular/common/http';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { DescontosPageRoutingModule } from './descontos-routing.module';
 
@@ -13,6 +17,8 @@ import { DescontosPage } from './descontos.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    HttpClientModule,
+    ExploreContainerComponentModule,
     DescontosPageRoutingModule
   ],
   declarations: [DescontosPage]
