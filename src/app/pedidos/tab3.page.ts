9843a5bca255b872b4916725b0dc4a27093eb199
The-CrazyMouse <andrerddantas@hotmail.com>
2025-06-02 20:07:06 +0100

    pedidos

--- src/app/pedidos/tab3.page.ts
+++ src/app/pedidos/tab3.page.ts
@@ -9,5 +9,32 @@ import { Component } from '@angular/core';
 export class Tab3Page {
 
   constructor() {}
+  pedidos = [
+    {
+      id: 1,
+      nome: 'Hambúrguer Clássico',
+      preco: 7.5,
+      imagem: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/1920px-RedDot_Burger.jpg',
+      quantidade: 1
+    },
+    {
+      id: 2,
+      nome: 'Pizza Margherita',
+      preco: 8.9,
+      imagem: 'https://safrescobaldistatic.blob.core.windows.net/media/2022/11/PIZZA-MARGHERITA.jpg',
+      quantidade: 1
+    },
+    // Adicione mais dinamicamente
+  ];
 
+  aumentar(item: any) {
+    item.quantidade++;
+  }
+
+  diminuir(item: any) {
+    if (item.quantidade > 1) {
+      item.quantidade--;
+    }
+  }
 }
+
